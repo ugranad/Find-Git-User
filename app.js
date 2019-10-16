51114ec45410928bd8f01ffc17bcbf8383f94009
Bharath Ugranad <46988525+ugranad@users.noreply.github.com>
2019-10-16 12:09:24 +0530

    Update app.js

--- app.js
+++ app.js
@@ -1,32 +1,31 @@
+// Init Github
 const github = new Github;
+// Init UI
 const ui = new UI;
 
-
-//Search Input
-
+// Search input
 const searchUser = document.getElementById('searchUser');
 
 // Search input event listener
 searchUser.addEventListener('keyup', (e) => {
-  //Get input text
+  // Get input text
   const userText = e.target.value;
 
-  if (userText !== '') {
-    // Make HTTP CALL 
-    github.getUser(userText)
-      .then(data => {
-        if (data.profile.message === 'Not Found') {
-          //Show alert
-          ui.showAlert('User not found', 'alert alert-danger');
-        } else {
-          //Show profile
-          ui.showProfile(data.profile);
-          ui.showRepos(data.repos);
-        }
-      })
+  if(userText !== ''){
+   // Make http call
+   github.getUser(userText)
+    .then(data => {
+      if(data.profile.message === 'Not Found') {
+        // Show alert
+        ui.showAlert('User not found', 'alert alert-danger');
+      } else {
+        // Show profile
+        ui.showProfile(data.profile);
+        ui.showRepos(data.repos);
+      }
+    })
   } else {
-    //Clear Profile
-    ui.clearProfile()
-
+    // Clear profile
+    ui.clearProfile();
   }
-})
+}); 
